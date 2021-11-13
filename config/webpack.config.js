9ece28ee7a731d3fd0cf5acc8d08064e0093b637
国服第一李师师 <31493533+MDGE@users.noreply.github.com>
2021-11-13 17:32:04 +0800

    Update webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -671,17 +671,17 @@ module.exports = function (webpackEnv) {
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       // Generate a service worker script that will precache, and keep up to date,
       // the HTML & assets that are part of the webpack build.
-      // isEnvProduction &&
-      //   fs.existsSync(swSrc) &&
-      //   new WorkboxWebpackPlugin.InjectManifest({
-      //     swSrc,
-      //     dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
-      //     exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
-      //     // Bump up the default maximum size (2mb) that's precached,
-      //     // to make lazy-loading failure scenarios less likely.
-      //     // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
-      //     maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      //   }),
+      isEnvProduction &&
+        fs.existsSync(swSrc) &&
+        new WorkboxWebpackPlugin.InjectManifest({
+          swSrc,
+          dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+          exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+          // Bump up the default maximum size (2mb) that's precached,
+          // to make lazy-loading failure scenarios less likely.
+          // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
+          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        }),
       new WorkboxWebpackPlugin.GenerateSW({
         // 这些选项帮助快速启用 ServiceWorkers
         // 不允许遗留任何“旧的” ServiceWorkers
